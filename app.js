09a080b7580ba4021bd3c348aea5f919c86f5b9f
myusername <you@example.com>
2021-02-15 16:22:36 +1100

    New Node JS project
    New Project

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res, next) => {
+    console.log('In the another middleware!');
+    res.send('<form action = "/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form>');
+});
+
+app.post('/product',(req, res, next) =>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next) => {
+    console.log('In the another middleware!');
+    res.send('<h1> Hello Form Express! </h1>');
+});
+
+app.listen(3000);
