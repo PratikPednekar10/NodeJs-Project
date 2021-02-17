f5a9ab90adf017e1939a36ab5c90577e6a610837
myusername <you@example.com>
2021-02-17 17:54:28 +1100

    New Push

--- routes/admin.js
+++ routes/admin.js
@@ -2,15 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/admin/add-product', (req, res, next) => {
-    res.send(
-        '<form action = "/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form>');
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
 });
 
-router.post('/admin/add-product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
-
 
 module.exports = router;
